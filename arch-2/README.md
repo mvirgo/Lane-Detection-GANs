90d3af7a2cbe1a32f918836a2ca84408fd141d13
Michael Virgo <mvirgs44@gmail.com>
2018-04-14 15:22:17 -0700

    Arch-2 batch norm testing results

--- arch-2/README.md
+++ arch-2/README.md
@@ -4,11 +4,11 @@ Check performance of the network by adding in batch normalization for all layers
 ## Summary
 1. Attempted models with each of these individually
    - No pooling (replaced by strides of 2x2 over max pooling)
-   - More batch normalization
+   - Batch normalization on every layer
    - Adding skip layers
    - Combination of the above
 
-**In progres**
+**In progress**
 
 ## Results
 
@@ -17,5 +17,6 @@ Check performance of the network by adding in batch normalization for all layers
 Arch | Test Acc | Speed | Parameters
 --- | --- | --- | ---
 No pooling | 0.8683 | 4.75 ms | 725,101
+Batch norm | 0.8692 | 12.04 ms | 728,749
 
 See also the included outputs from the challenge video.
---
+++ arch-2/fully_conv_NN_batch_norm.py
@@ -0,0 +1,159 @@
+""" This file contains code for a fully convolutional
+(i.e. contains zero fully connected layers) neural network
+for detecting lanes. This version assumes the inputs
+to be road images in the shape of 112 x 112 x 3 (RGB) with
+the labels as 112 x 112 x 1 (just the G channel with a
+re-drawn lane). Note that in order to view a returned image,
+the predictions is later stacked with zero'ed R and B layers
+and added back to the initial road image.
+"""
+
+import numpy as np
+import pickle
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+# Import necessary items from Keras
+from keras.models import Sequential
+from keras.layers import Activation, Dropout, UpSampling2D
+from keras.layers import Conv2DTranspose, Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from keras import regularizers
+
+# Load training images
+train_images = pickle.load(open("full_CNN_train112.p", "rb" ))
+
+# Load image labels
+labels = pickle.load(open("full_CNN_labels112.p", "rb" ))
+
+# Make into arrays as the neural network wants these
+train_images = np.array(train_images)
+labels = np.array(labels)
+
+# Normalize labels - training images get normalized to start in the network
+labels = labels / 255
+
+# Shuffle images along with their labels, then split into training/validation sets
+train_images, labels = shuffle(train_images, labels)
+# Test size may be 10% or 20%
+X_train, X_val, y_train, y_val = train_test_split(train_images, labels, test_size=0.1)
+
+# Batch size, epochs and pool size below are all paramaters to fiddle with for optimization
+batch_size = 64
+epochs = 10
+pool_size = (2, 2)
+input_shape = X_train.shape[1:]
+
+### Here is the actual neural network ###
+model = Sequential()
+# Normalizes incoming inputs. First layer needs the input shape to work
+model.add(BatchNormalization(input_shape=input_shape))
+
+# Below layers were re-named for easier reading of model summary; this not necessary
+# Conv Layer 1
+model.add(Conv2D(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv1'))
+model.add(BatchNormalization())
+
+# Conv Layer 2
+model.add(Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv2'))
+model.add(BatchNormalization())
+
+# Pooling 1
+model.add(MaxPooling2D(pool_size=pool_size))
+
+# Conv Layer 3
+model.add(Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv3'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Conv Layer 4
+model.add(Conv2D(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv4'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Conv Layer 5
+model.add(Conv2D(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv5'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Pooling 2
+model.add(MaxPooling2D(pool_size=pool_size))
+
+# Conv Layer 6
+model.add(Conv2D(128, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv6'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Conv Layer 7
+model.add(Conv2D(128, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv7'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Pooling 3
+model.add(MaxPooling2D(pool_size=pool_size))
+
+# Upsample 1
+model.add(UpSampling2D(size=pool_size))
+
+# Deconv 1
+model.add(Conv2DTranspose(128, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv1'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Deconv 2
+model.add(Conv2DTranspose(128, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv2'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Upsample 2
+model.add(UpSampling2D(size=pool_size))
+
+# Deconv 3
+model.add(Conv2DTranspose(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv3'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Deconv 4
+model.add(Conv2DTranspose(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv4'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Deconv 5
+model.add(Conv2DTranspose(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv5'))
+model.add(BatchNormalization())
+model.add(Dropout(0.2))
+
+# Upsample 3
+model.add(UpSampling2D(size=pool_size))
+
+# Deconv 6
+model.add(Conv2DTranspose(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv6'))
+model.add(BatchNormalization())
+
+# Final layer - only including one channel so 1 filter
+model.add(Conv2DTranspose(1, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Final'))
+
+### End of network ###
+
+
+# Using a generator to help the model use less data
+# Channel shifts help with shadows slightly
+datagen = ImageDataGenerator(channel_shift_range=0.2)
+datagen.fit(X_train)
+
+# Compiling and training the model
+model.compile(optimizer='Adam', loss='mean_squared_error', metrics=['accuracy'])
+model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size), steps_per_epoch=len(X_train)/batch_size,
+epochs=epochs, verbose=1, validation_data=(X_val, y_val))
+
+# Freeze layers since training is done
+model.trainable = False
+model.compile(optimizer='Adam', loss='mean_squared_error')
+
+# Save model architecture and weights
+model.save('full_CNN_model112_2_batch_norm.h5')
+
+# Show summary of model
+model.summary()
+
